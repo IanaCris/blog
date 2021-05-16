07c52e8db806a1e8d8961bb713c3be77a807ecc1
IanaCris <ianasousati@gmail.com>
2021-05-15 23:22:49 -0300

    prismic

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,8 @@
 import { GetStaticProps } from 'next';
 
 import { getPrismicClient } from '../services/prismic';
+import Prismic from '@prismicio/client';
+import { RichText } from 'prismic-dom';
 
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
@@ -24,15 +26,35 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-export default function Home() {
+export default function Home({posts}) {
+  console.log(posts);
   return (
     <h1>Home do blog</h1>
   )
 }
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient();
-//   // const postsResponse = await prismic.query(TODO);
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+  const postsResponse = await prismic.query([
+      Prismic.predicates.at('document.type', 'posts')
+    ], {
+      fetch: ['posts.title', 'posts.content'],
+      pageSize: 20,
+    }
+  );
 
-//   // TODO
-// };
+  console.log(postsResponse);
+
+  const posts = postsResponse.results.map(post => {
+    return {
+      slug: post.uid,
+      title: RichText.asText(post.data.title),
+    };
+  });
+
+  return {
+    props: {
+      posts
+    }
+  }
+};
